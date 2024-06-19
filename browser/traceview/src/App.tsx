d02d6d91bbe6329142a9f0e60ebd0d53b1fa4ba3
Ada Bohm <ada@kreatrix.org>
2024-06-19 17:27:39 +0200

    WIP

--- browser/traceview/src/App.tsx
+++ browser/traceview/src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 import { TreeView } from './components/TreeView'
 import { TracingNode } from './model/Node'
 
+export interface TreeState {
+  opened: Set<string>
+  selected: TracingNode
+}
+
 function App(props: { root: TracingNode }) {
+  const [state, setState] = useState<TreeState>({ opened: new Set, selected: props.root });
   return (
     <div className="container">
-      <div className='panel'><TreeView /></div>
+      <div className='panel'><TreeView root={props.root} treeState={state} setTreeState={setState} /></div>
       <div className='content'>XXXX</div>
     </div>
   )
--- browser/traceview/src/components/TreeView.tsx
+++ browser/traceview/src/components/TreeView.tsx
@@ -1,34 +1,60 @@
+import { TreeState } from "../App";
+import { TracingNode } from "../model/Node"
 import "./TreeView.css"
+import { FaCaretDown, FaCaretRight } from "react-icons/fa6"
 
+function TreeNode(props: { node: TracingNode, treeState: TreeState, setTreeState: (n: TreeState) => void }) {
+    const node = props.node;
+    const isSelected = node.uid === props.treeState.selected.uid;
+    const isOpen = props.treeState.opened.has(node.uid);
+    let children;
+    if (isOpen && node.children && node.children.length > 0) {
+        children = <ul>{node.children.map((c) => <TreeNode node={c} treeState={props.treeState} setTreeState={props.setTreeState} />)}</ul>;
+    }
 
-export function TreeView() {
+    const onSelect = (event: React.MouseEvent<unknown>) => {
+        props.setTreeState({
+            ...props.treeState,
+            selected: node,
+        })
+        event.preventDefault();
+        event.stopPropagation();
+    };
+
+    const onToggle = (event: React.MouseEvent<unknown>) => {
+        const clonedSet = new Set(props.treeState.opened);
+        if (isOpen) {
+            clonedSet.delete(node.uid);
+        } else {
+            clonedSet.add(node.uid);
+        }
+
+        props.setTreeState({
+            ...props.treeState,
+            opened: clonedSet,
+        })
+
+        event.stopPropagation()
+        event.preventDefault()
+    };
+
+    let icon;
+    if (node.children) {
+        if (!isOpen) {
+            icon = <FaCaretRight className="icon" onClick={onToggle} />
+        } else {
+            icon = <FaCaretDown className="icon" onClick={onToggle} />
+        }
+    }
+
+    return (<li><div className={(isSelected ? "selected" : "") + " box"} onClick={onSelect}>
+        {icon}<span>{node.name}</span></div><div className="tree-children">{children}</div></li>)
+}
+
+export function TreeView(props: { root: TracingNode, treeState: TreeState, setTreeState: (n: TreeState) => void }) {
     return (<div className="tree">
         <ul>
-            <li>
-                Fruit
-                <ul>
-                    <li>
-                        Red
-                        <ul>
-                            <li>Cherry</li>
-                            <li>Strawberry</li>
-                        </ul>
-                    </li>
-                    <li>
-                        Yellow
-                        <ul>
-                            <li>Banana</li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                Meat
-                <ul>
-                    <li>Beef</li>
-                    <li>Pork</li>
-                </ul>
-            </li>
+            <TreeNode node={props.root} treeState={props.treeState} setTreeState={props.setTreeState} />
         </ul>
     </div>);
 }
