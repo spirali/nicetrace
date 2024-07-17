1f75feedd62c4c59915dd139d74b6dcead1aea85
Ada Bohm <ada@kreatrix.org>
2024-07-17 18:42:36 +0200

    Colab support updated

--- src/nicetrace/server/app.py
+++ src/nicetrace/server/app.py
@@ -5,7 +5,7 @@ from ..reader.base import TraceReader
 from ..html.staticfiles import read_index, STATIC_FILE_DIR
 
 
-def create_app(reader: TraceReader, port):
+def create_app(reader: TraceReader, server_name):
     app = Flask(__name__, static_url_path="/assets", static_folder=STATIC_FILE_DIR)
     CORS(app)
 
@@ -21,26 +21,36 @@ def create_app(reader: TraceReader, port):
     @app.route("/")
     def get_index(trace_id: str | None = None):
         content = read_index()
-        content = content.replace("%%%URL%%%", f"http://localhost:{port}/")
+        content = content.replace("%%%URL%%%", server_name)
         return content
 
     return app
 
 
 def start_server(
-    reader: TraceReader, host: str = "localhost", port: int = 4090, debug: bool = False
+    reader: TraceReader,
+    host: str = "localhost",
+    server_name: str = "localhost",
+    port: int = 4090,
+    debug: bool = False,
+    verbose: bool = True,
 ):
     """
     This needs feature "server".
     Starts a HTTP server over a given trace reader. It blocks the process.
     """
-    application = create_app(reader, port)
+    if server_name is None:
+        server_name = f"http://localhost:{port}/"
+    elif not server_name.endswith("/"):
+        server_name += "/"
+    application = create_app(reader, server_name)
     if debug:
         application.run(host=host, port=port, debug=True)
     else:
         from waitress import serve
 
-        print(f"Running at http://localhost:{port}")
+        if verbose:
+            print(f"Running at {server_name}")
         serve(application, host=host, port=port)
 
 
@@ -52,5 +62,36 @@ def start_server_in_jupyter(reader: TraceReader, port: int = 4090, debug: bool =
 
     from IPython.lib import backgroundjobs as bg
 
+    try:
+        # Running in Google Colab
+        from google.colab.output import eval_js
+
+        server_name = str(eval_js(f"google.colab.kernel.proxyPort({port})"))
+        host = "0.0.0.0"
+    except ImportError:
+        # Running in normal jupyter notebook
+        server_name = None
+        host = "localhost"
     jobs = bg.BackgroundJobManager()
-    jobs.new(lambda: start_server(reader, port=port, debug=debug))
+    jobs.new(
+        lambda: start_server(
+            reader, port=port, debug=debug, server_name=server_name, host=host
+        )
+    )
+
+    # if inline_colab:
+    #     import IPython.display
+    #
+    #     return IPython.display.Javascript(
+    #         """
+    #         (async ()=>{{
+    #             fm = document.createElement('iframe')
+    #             fm.src = await google.colab.kernel.proxyPort({port})
+    #             fm.width = '90%'
+    #             fm.height = '1024'
+    #             fm.frameBorder = 0
+    #             fm.style = 'background: white;'
+    #             document.body.append(fm)
+    #         }})();
+    #     """.format(port=port)
+    #     )
